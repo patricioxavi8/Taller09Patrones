52bf242c8ad16fba44182ab5947c3ab758f33f1b
patricioxavi <pjtovar@espol.edu.ec>
2018-12-17 10:58:27 -0500

    add archivos

---
+++ Facturador/src/estructural/Esquema.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructural;
+import  creacional.ComprobanteElectronico;
+
+/**
+ *
+ * @author Patricio
+ */
+public interface Esquema {
+    
+    public void autorizar(ComprobanteElectronico comprobante);
+    
+}
